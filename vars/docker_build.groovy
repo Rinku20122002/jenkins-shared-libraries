6fc2e82d9b735322bde1b8d6a20673eb609d108b
Rinku20122002 <ry134527@gmail.com>
2026-06-17 18:20:25 +0530

    Fix Docker user variable in build command

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,3 +1,3 @@
 def call(String ProjectName, String ImageTag, String DockerUser){
-  sh"docker build -t ${DockerHubUser}/${ProjectName}":${ImageTag} ."
+    sh "docker build -t ${DockerUser}/${ProjectName}:${ImageTag} ."
 }
